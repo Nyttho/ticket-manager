a7a5e4873ae93571593de9e89e1bd3764b60d5ec
Nyttho <anthony.dms@outlook.com>
2025-06-03 14:11:01 +0200

    ticket entity

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tickets')]
+    private ?User $owner = null;
+
+    #[ORM\ManyToOne]
+    private ?User $assignee = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $first_assigned_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $last_assigned_at = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $priority = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $state = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getAssignee(): ?User
+    {
+        return $this->assignee;
+    }
+
+    public function setAssignee(?User $assignee): static
+    {
+        $this->assignee = $assignee;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getFirstAssignedAt(): ?\DateTimeImmutable
+    {
+        return $this->first_assigned_at;
+    }
+
+    public function setFirstAssignedAt(\DateTimeImmutable $first_assigned_at): static
+    {
+        $this->first_assigned_at = $first_assigned_at;
+
+        return $this;
+    }
+
+    public function getLastAssignedAt(): ?\DateTimeImmutable
+    {
+        return $this->last_assigned_at;
+    }
+
+    public function setLastAssignedAt(\DateTimeImmutable $last_assigned_at): static
+    {
+        $this->last_assigned_at = $last_assigned_at;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriority(): ?string
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(string $priority): static
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
